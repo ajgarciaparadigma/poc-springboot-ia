bbdc82868bb34a5d55ac3f4acccce21796867251
ajgarcia <ajgarcia@paradigmadigital.com>
2024-05-31 09:38:40 +0200

    return image byte[]

--- src/main/java/com/paradigma/poc/example_ia/controllers/OpenAIController.java
+++ src/main/java/com/paradigma/poc/example_ia/controllers/OpenAIController.java
@@ -4,11 +4,13 @@ package com.paradigma.poc.example_ia.controllers;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.image.ImageOptions;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiAudioSpeechModel;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.ai.openai.audio.speech.SpeechMessage;
 import org.springframework.ai.openai.audio.speech.SpeechPrompt;
 import org.springframework.ai.openai.audio.speech.SpeechResponse;
@@ -22,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.util.Base64;
 import java.util.Map;
 
 
@@ -52,15 +55,22 @@ public class OpenAIController {
         return chatModel.stream(prompt);
     }
 
-    @GetMapping(value = "/ai/generateImage")
-    public String getImage(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
 
-        ImagePrompt prompt = new ImagePrompt(message);
+    @GetMapping(value = "/ai/generateImage", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> getImage(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        //
+        ImagePrompt prompt = new ImagePrompt(message, OpenAiImageOptions.builder().withResponseFormat("b64_json").build());
+
         ImageResponse imageResponse =  imageModel.call(prompt);
 
-        String imageUrl = imageResponse.getResults().get(0).getOutput().getUrl();
-        return "redirect:"+imageUrl;
+        String base64Image = imageResponse.getResults().get(0).getOutput().getB64Json();
+        byte[] imageBytes = Base64.getDecoder().decode(base64Image);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+
+        return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
     }
+
 
     @GetMapping(value = "/ai/generateAudio")
     public ResponseEntity<byte[]> getAudio(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
